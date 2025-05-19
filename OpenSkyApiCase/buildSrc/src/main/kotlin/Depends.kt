d42df967b0932bad1b64b180edd0b06c86f94115
AdemogluFurkan <ademoglu.furkan01@gmail.com>
2025-05-19 19:18:35 +0300

    remove unnucessary imports

--- OpenSkyApiCase/buildSrc/src/main/kotlin/Depends.kt
+++ OpenSkyApiCase/buildSrc/src/main/kotlin/Depends.kt
@@ -1,3 +1,3 @@
 object Depends {
-    const val BASE_URL = "\"https://{furkannademoglu:Test1234.}@opensky-network.org/api/\""
+    const val BASE_URL = "\"https://{furkannademogludeneme:Test1234.}@opensky-network.org/api/\""
 }
